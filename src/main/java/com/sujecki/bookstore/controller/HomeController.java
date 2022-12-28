c3c8ffd8992f19a48a1fadb0532533fa053a4f88
Konrad Sujecki <konradsujecki@MacBook-Pro-Konrad.local>
2022-12-28 13:40:46 +0100

    First commit, JWT authentication

---
+++ src/main/java/com/sujecki/bookstore/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.sujecki.bookstore.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String getHome(){
+        return "Home Page";
+    }
+
+    @GetMapping("/")
+    public String getDefault(){
+        return "Defaullt";
+    }
+}
---
+++ src/main/java/com/sujecki/bookstore/model/ERole.java
@@ -0,0 +1,7 @@
+package com.sujecki.bookstore.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
